c541f2f9f7d6267f140b993d2d15c4e65043ef1f
Brylie Christopher Oxley <brylie@amble.fi>
2019-09-13 15:00:30 +0300

    Make more Pythonic and avoid shadowing filter

---
+++ data_sieve/sift.py
@@ -0,0 +1,22 @@
+def sift(datum, sieve):
+    """
+    Sift properties of a given dict using a Boolean sieve.
+    :param datum: the dict to recursively filter.
+    :type datum: dict
+    :param sieve: a dict indicating which datum fields to keep
+    :type sieve: dict
+    :return: result
+    :rtype: dict
+    """
+
+    result = {}
+
+    for key, value in datum.items():
+        if key not in sieve:
+            continue
+        elif isinstance(value, dict):
+            result[key] = sift(value, sieve[key])
+        elif isinstance(value, list):
+            result[key] = [sift(item, sieve[key]) for item in value]
+
+    return result
